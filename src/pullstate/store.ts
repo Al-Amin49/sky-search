7bc3f6f40db4d24ef94ea28bc90c368377697671
Al-Amin49 <alamin496832@gmail.com>
2024-10-31 22:30:46 +0600

    feat: added pullstate

---
+++ src/pullstate/store.ts
@@ -0,0 +1,25 @@
+import { Store } from "pullstate";
+
+type Travelers = { adult: number; child: number; infant: number };
+
+type FlightSearchState= {
+  journeyType: string;
+  travelers: Travelers;
+  cabinClass: string;
+  flightType: string;
+  baggageOption: string;
+  departure: string;
+  arrival: string;
+  departureDate: Date | null;
+}
+
+export const FlightSearchStore = new Store<FlightSearchState>({
+  journeyType: "OneWay",
+  travelers: { adult: 1, child: 0, infant: 0 },
+  cabinClass: "Economy",
+  flightType: "any",
+  baggageOption: "any",
+  departure: "",
+  arrival: "",
+  departureDate: null,
+});
